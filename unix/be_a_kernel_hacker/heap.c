ce7b04e8a42658a79ed0be7fcfa6aaf663784ce6
DeathNik <a.e.nikolaichik@gmail.com>
2016-01-20 15:47:37 +0300

    some fixes for heap

--- unix/be_a_kernel_hacker/heap.c
+++ unix/be_a_kernel_hacker/heap.c
@@ -31,33 +31,70 @@ struct heap {
 	char ** data;
 } ;
 
-void heap_pop(struct heap *h){
-	if(h->cur_size > 0 ){
-		char * tmp = h->data[0];
-		h->data[0] = h->data[h->cur_size];
-		h->data[h->cur_size] = tmp;
-		--h->cur_size;
-	}
+#define PARENT(id) ((id-1)/2)
+#define LEFT_CHILD(id) ((2 * id) + 1)
+#define RIGHT_CHILD(id) ((2 * id) + 2)
+
+//---- heap struct operations
+
+void heap_pop(struct heap *h) {
+    if (h->cur_size > 0) {
+        char *tmp = h->data[0];
+        h->data[0] = h->data[h->cur_size - 1];
+        h->data[h->cur_size-1] = tmp;
+        --h->cur_size;
+    }
+    // fix heap invariant
+    if (h->cur_size > 0) {
+        int id = 0;
+        int lchild, rchild, min_ind;
+        char *tmp;
+        while (true) {
+            lchild = LEFT_CHILD(id);
+            rchild = RIGHT_CHILD(id);
+            if (rchild >= h->cur_size) {
+                if (lchild >= h->cur_size)
+                    break;
+                else
+                    min_ind = lchild;
+            } else {
+                if (-strcmp(h->data[lchild], h->data[rchild]) <= 0)
+                    min_ind = lchild;
+                else
+                    min_ind = rchild;
+            }
+
+            if (-strcmp(h->data[id], h->data[min_ind]) > 0) {
+                tmp = h->data[min_ind];
+                h->data[min_ind] = h->data[id];
+                h->data[id] = tmp;
+                id = min_ind;
+            } else {
+                break;
+            }
+
+        }
+    }
 }
 
 
-static void process_insert(struct heap *h){
-	int i = h->cur_size;
-	int parent = (i-1)/2;
-	while( i > 0 && strcmp(h->data[parent],h->data[i]) >=0){
-		char * tmp = h->data[i];
-		h->data[i] = h->data[parent];
-		h->data[parent] = tmp;
+//fix heap inv after pop
+static void process_insert(struct heap *h) {
+    int i = h->cur_size;
+    int parent = PARENT(i);
+    while (i > 0 && -strcmp(h->data[parent], h->data[i]) >= 0) {
+        char *tmp = h->data[i];
+        h->data[i] = h->data[parent];
+        h->data[parent] = tmp;
 
-		i = parent;
-		parent = (i-1)/2;
-	}
-	if(h->cur_size < h->max_size)
-		++h->cur_size;
+        i = parent;
+        parent = PARENT(i);
+    }
+    if (h->cur_size < h->max_size)
+        ++h->cur_size;
 }
 
-static ssize_t print_heap(struct heap * h, char __user * out,
-			    size_t size, size_t readed){
+static ssize_t print_heap(struct heap * h, char __user * out, size_t size, size_t readed){
 	if ( h->cur_size == 0  || size == 0){
 		return readed;
 	}
@@ -68,9 +105,10 @@ static ssize_t print_heap(struct heap * h, char __user * out,
 	heap_pop(h);
 	return print_heap(h,out,size-copy_len,readed+copy_len);
 }
+//---- END of heap struct operations
 
 
-
+//---- start of module functions
 static struct heap * heap_alloc(unsigned long element_size){
 	struct heap * h = kzalloc(sizeof(struct heap),GFP_KERNEL);
 	if (unlikely(!h)){
@@ -81,13 +119,13 @@ static struct heap * heap_alloc(unsigned long element_size){
 
 	//allocate max_size +1 buffs for string storage
 	// +1 will be used as temp
-	h->data = kzalloc(h->max_size +1 , GFP_KERNEL);
+	h->data = kzalloc((h->max_size + 1) * sizeof(char*) , GFP_KERNEL);
 	if (unlikely(!h->data)){
 		kfree(h);
 		return NULL;
 	}
 	for(int i=0; i< h->max_size +1; ++i){
-		h->data[i] = kzalloc(_element_size, GFP_KERNEL);
+		h->data[i] = kzalloc(_element_size *sizeof(char), GFP_KERNEL);
 		if (unlikely(!h->data[i])){
 			for(int j =0; j <i; ++j)
 				kfree(h->data[j]);
@@ -124,17 +162,21 @@ static int heap_open(struct inode *inode, struct file *file){
 
 static ssize_t heap_read(struct file *file, char __user * out,
 			    size_t size, loff_t * off){
+	printk(KERN_INFO"heap read call");
 	struct heap * h = file->private_data;
 	if (mutex_lock_interruptible(&h->lock))
 		return -ERESTARTSYS;
 
 	ssize_t result = print_heap(h,out,size,0);
 	mutex_unlock(&h->lock);
+	printk(KERN_INFO"heap read done ok");
 	return result;
 }
 
+//called on heap_write
 static ssize_t heap_write(struct file *file, const char __user * in,
 			     size_t size, loff_t * off){
+	//printk(KERN_INFO"heap write call\n");
 	struct heap * h = file->private_data;
 	if(size > _element_size)
 		return -EFBIG;
@@ -142,21 +184,27 @@ static ssize_t heap_write(struct file *file, const char __user * in,
 	if (mutex_lock_interruptible(&h->lock))
 		return -ERESTARTSYS;
 
+    if(h->cur_size == h->max_size)
+          heap_pop(h);
+
 	if (copy_from_user(h->data[h->cur_size],in, size))
 		return - EFAULT;
 
 	process_insert(h);
 
 	mutex_unlock(&h->lock);
+	//printk(KERN_INFO"heap write done ok %i\n",h->cur_size);
 	return size;
 }
 
+//free used mem
 static int heap_close(struct inode *inode, struct file *file){
 	struct heap *h = file->private_data;
 	heap_free(h);
 	return 0;
 }
 
+//standart template
 static struct file_operations heap_fops = {
 	.owner = THIS_MODULE,
 	.open = heap_open,
@@ -171,7 +219,6 @@ static struct miscdevice heap_misc_device = {
 	.name = "heap",
 	.fops = &heap_fops
 };
-
 
 static int __init heap_init(void){
 	if (!_element_size) return -1;
