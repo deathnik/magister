e04346721f44993c392a1d14a35c9accf0d2b254
DeathNik <a.e.nikolaichik@gmail.com>
2016-01-20 16:12:36 +0300

    fix

--- unix/4_be_a_library_hijacker/memcpy.c
+++ unix/4_be_a_library_hijacker/memcpy.c
@@ -8,8 +8,8 @@
 
 void *memcpy(void * dst, const void * src, size_t n){
   fputs("memcpy: our reloaded version called\n", stdout);
-  if ( (src <= dst and dst < src + n) or
-  	   (src <= dst + n -1 and dst + n - 1 < src + n) ){
+  if ( (src <= dst && dst < src + n) ||
+  	   (src <= dst + n -1 && dst + n - 1 < src + n) ){
   	fputs("memcpy: src == dst, falling to memmove", stdout);
   	return memmove(dst,src,n);
   }
