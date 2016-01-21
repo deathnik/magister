357368ece3565a78758e3699aa2b4115df382ecb
DeathNik <a.e.nikolaichik@gmail.com>
2016-01-21 15:45:11 +0300

    some impr in voice and fixes

--- unix/voice/voice.sh
+++ unix/voice/voice.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ -z "$1" ] || [ "$1" == "-h" ]; then
     echo "Usage: $0 <path_to_file_with_data>" 1>&2
@@ -6,22 +6,33 @@ if [ -z "$1" ] || [ "$1" == "-h" ]; then
 fi
 
 filename=$1
+tmp_name=$(mktemp)
+
+# get data if file missing
+if [ ! -f "$filename" ]
+then
+    filename=$tmp_name
+    wget https://raw.githubusercontent.com/xbmc/xbmc/master/LICENSE.GPL -q -O "$filename"
+fi
 
 log="errors.log"
 echo "" > $log
+
+ok=0
+neok=0
 
 say_something(){
 	festival -b "(begin  (SayText \"$1\" nil))" ;
 }
 
 clean_string(){
-	a=$(echo "$1" | tr -d '[:punct:]' | tr -s ' ')
+	a=$(echo "$1" | tr '[:punct:]' ' ' | tr -s ' ')
 	echo "${a,,}"
 }
 
 on_ok(){
-	say_something "Nice done!"
-
+	say_something "Well done!"
+	ok=$((ok+1))
 }
 
 on_error(){
@@ -31,33 +42,52 @@ on_error(){
 	echo "Expected: $expected" >> $log
 	say_something "Typed: $typed"
 	echo "Typed: $typed" >> $log
+	neok=$((neok+1))
 }
 
 
 welcome="Type the phrases you hear. Type a single question mark character to repeat, empty line to finish"
-#say_something "$welcome"
+say_something "$welcome"
 
 while true; do
-	line=$(shuf -n 1 $filename)
+	# get random line
+	line=$(shuf -n 1 "$filename")
 	while true ; do
 		say_something "$line" ;
-       	read -r answer </dev/tty
-       	if [ " $answer" != " ?" ];
+		#do not show typed text
+       	read -s -r answer </dev/tty
+       	if [ "$answer" != "?" ];
        	then
        		break;
        	fi
     done
-    if [ " $answer" == " " ];
+    if [ "$answer" == "" ];
     then
     	break;
     fi
-
+    #process answer
     clean_answer=$(clean_string "$answer") ;
     clean_line=$(clean_string "$line") ;
-    if [ " $clean_answer" == " $clean_line" ];
+    if [ "$clean_answer" == "$clean_line" ];
     then
     	on_ok
     else
     	on_error "$clean_line" "$clean_answer"
     fi
 done
+
+#print error stat
+sum=$((ok + neok))
+if [ $sum -gt 0 ]
+then
+  errors_rate=$(bc <<< "scale=2; 100*$neok/($ok+$neok)")
+  printf "\nErrors rate:$errors_rate  %%\n"
+else
+  printf "\nNo input done\n"
+fi
+
+# remove downloaded data
+if [ "$filename" == "$tmp_name" ]
+then
+    rm "$tmp_name"
+fi
